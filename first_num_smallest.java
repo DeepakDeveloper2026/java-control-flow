52d476a9e15a8edb1bd34115385fd2c3c4273318
DeepakDeveloper2026 <dy5055290@gmail.com>
2025-06-11 12:17:08 +0530

    commited

---
+++ first_num_smallest.java
@@ -0,0 +1,11 @@
+public class first_num_smallest {
+public static void main(String[] args) {
+    int num1=1;
+    int num2=3;
+    int num3=4;
+    if((num1<num2 && num1<num3))
+    System.out.print(num1+" is smallest");
+    else
+    System.out.print(num1+" is not smallest");
+}
+}
